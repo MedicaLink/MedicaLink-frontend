ab8bde7df035bad54fc2905f34bddaf57e1e6df6
Nisala Dulanaka <dulanakanisala654@gmail.com>
2024-08-14 23:46:19 +0530

    Added the docker file

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Set your desired build output directory
+    outDir: 'build', // Set your desired build output directory
   },
 })
